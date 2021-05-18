1ac245ac5e77d28349f6f386512c3584be1a7fb9
Jhonatan Scremim <jhonatanscremim@gmail.com>
2021-05-17 22:57:57 -0300

    feat(create-project): cria projeto inicial

---
+++ AwsTranscribeVocabulary/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿
+namespace AwsTranscribeVocabulary
+{
+    partial class FormMain
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TextBox = new System.Windows.Forms.TextBox();
+            this.comboBoxBucket = new System.Windows.Forms.ComboBox();
+            this.BtSearch = new System.Windows.Forms.Button();
+            this.BtJob = new System.Windows.Forms.Button();
+            this.BtVocabularyFilter = new System.Windows.Forms.Button();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // TextBox
+            // 
+            this.TextBox.Location = new System.Drawing.Point(98, 39);
+            this.TextBox.Name = "TextBox";
+            this.TextBox.Size = new System.Drawing.Size(432, 27);
+            this.TextBox.TabIndex = 0;
+            // 
+            // comboBoxBucket
+            // 
+            this.comboBoxBucket.FormattingEnabled = true;
+            this.comboBoxBucket.Location = new System.Drawing.Point(98, 93);
+            this.comboBoxBucket.Name = "comboBoxBucket";
+            this.comboBoxBucket.Size = new System.Drawing.Size(293, 28);
+            this.comboBoxBucket.TabIndex = 1;
+            // 
+            // BtSearch
+            // 
+            this.BtSearch.Location = new System.Drawing.Point(545, 37);
+            this.BtSearch.Name = "BtSearch";
+            this.BtSearch.Size = new System.Drawing.Size(94, 29);
+            this.BtSearch.TabIndex = 2;
+            this.BtSearch.Text = "BtSearch";
+            this.BtSearch.UseVisualStyleBackColor = true;
+            this.BtSearch.Click += new System.EventHandler(this.BtSearch_Click);
+            // 
+            // BtJob
+            // 
+            this.BtJob.Location = new System.Drawing.Point(436, 93);
+            this.BtJob.Name = "BtJob";
+            this.BtJob.Size = new System.Drawing.Size(94, 29);
+            this.BtJob.TabIndex = 3;
+            this.BtJob.Text = "BtJob";
+            this.BtJob.UseVisualStyleBackColor = true;
+            this.BtJob.Click += new System.EventHandler(this.BtJob_Click);
+            // 
+            // BtVocabularyFilter
+            // 
+            this.BtVocabularyFilter.Location = new System.Drawing.Point(98, 161);
+            this.BtVocabularyFilter.Name = "BtVocabularyFilter";
+            this.BtVocabularyFilter.Size = new System.Drawing.Size(94, 29);
+            this.BtVocabularyFilter.TabIndex = 4;
+            this.BtVocabularyFilter.Text = "BtVocabularyFilter";
+            this.BtVocabularyFilter.UseVisualStyleBackColor = true;
+            this.BtVocabularyFilter.Click += new System.EventHandler(this.BtVocabularyFilter_Click);
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.FileName = "openFileDialog1";
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(798, 450);
+            this.Controls.Add(this.BtVocabularyFilter);
+            this.Controls.Add(this.BtJob);
+            this.Controls.Add(this.BtSearch);
+            this.Controls.Add(this.comboBoxBucket);
+            this.Controls.Add(this.TextBox);
+            this.Name = "FormMain";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox TextBox;
+        private System.Windows.Forms.Button BtVocabularyFilter;
+        private System.Windows.Forms.Button BtJob;
+        private System.Windows.Forms.Button BtSearch;
+        private System.Windows.Forms.ComboBox comboBoxBucket;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+    }
+}
+
---
+++ AwsTranscribeVocabulary/Form1.cs
@@ -0,0 +1,148 @@
+﻿using Amazon;
+using Amazon.Runtime;
+using Amazon.Runtime.CredentialManagement;
+using Amazon.S3;
+using Amazon.S3.Model;
+using Amazon.S3.Transfer;
+using Amazon.TranscribeService;
+using Amazon.TranscribeService.Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AwsTranscribeVocabulary
+{
+    public partial class FormMain : Form
+    {
+        AWSCredentials credentials;
+        private static readonly RegionEndpoint region = RegionEndpoint.USEast1;
+        private string vocabularyFilter;
+        AmazonS3Client client;
+        public FormMain()
+        {
+            InitializeComponent();
+            comboBoxBucket.Items.Clear();
+            var listBuckets = S3ListBuckets();
+            foreach (var item in listBuckets)
+            {
+                comboBoxBucket.Items.Add(item);
+            }
+        }
+
+        private void GetCredentials()
+        {
+            var chain = new CredentialProfileStoreChain();
+            if (!chain.TryGetAWSCredentials("AWS Educate", out credentials))
+            {
+                MessageBox.Show("Erro ao pegar credencial");
+            }
+        }
+
+        private void S3Client()
+        {
+            GetCredentials();
+            client = new AmazonS3Client(credentials, region);
+        }
+
+        private List<string> S3ListBuckets()
+        {
+            var buckets = new List<string>();
+            S3Client();
+            ListBucketsResponse listBuckets = (client.ListBucketsAsync()).Result;
+            foreach (S3Bucket i in listBuckets.Buckets)
+            {
+                buckets.Add(i.BucketName);
+            }
+            return buckets;
+        }
+
+        private async Task<bool> UpLoadToS3(string file)
+        {
+            GetCredentials();
+            try
+            {
+                var s3Client = new AmazonS3Client(credentials, region);
+                var fileTransferUtility = new TransferUtility(s3Client);
+                await fileTransferUtility.UploadAsync(file, comboBoxBucket.SelectedItem.ToString());
+                return true;
+            }
+            catch (AmazonS3Exception e)
+            {
+                MessageBox.Show($"Erro: {e.Message}");
+                return false;
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show($"Erro: {e.Message}");
+                return false;
+            }
+        }
+
+        private async Task ExecuteTranscribeAsync()
+        {
+            AmazonTranscribeServiceClient transcribeServiceClient = new AmazonTranscribeServiceClient(credentials, region);
+            string jobName = $"Trascribe-Reuniao-{DateTime.Now.ToString("yyyymmddhhmmss")}";
+            StartTranscriptionJobRequest jobRequest = new StartTranscriptionJobRequest
+            {
+                Media = new Media { MediaFileUri = "s3://" + "my-transcribe-bucket-input/" + Path.GetFileName(openFileDialog.FileName) },
+                OutputBucketName = comboBoxBucket.SelectedItem.ToString(),
+                LanguageCode = LanguageCode.PtBR,
+                MediaFormat = MediaFormat.Mp4,
+                TranscriptionJobName = jobName,
+                Settings = new Settings()
+                {
+                    VocabularyFilterName = vocabularyFilter
+                }
+            };
+
+            await transcribeServiceClient.StartTranscriptionJobAsync(jobRequest);
+        }
+
+        private async Task ExecuteVocabularyFilter()
+        {
+            var client = new AmazonTranscribeServiceClient(credentials, region);
+            string vocabularyFilterName = $"my-audio-bucket-001-{DateTime.Now.ToString("yyyymmddhhmmss")}";
+            var createVocabularyFilter = new CreateVocabularyFilterRequest()
+            {
+                LanguageCode = LanguageCode.PtBR,
+                VocabularyFilterName = vocabularyFilterName,
+                Words = { "beleza", "hoje", "daqui", "encher" }
+            };
+
+            await client.CreateVocabularyFilterAsync(createVocabularyFilter);
+
+            var getRequest = new GetVocabularyFilterRequest()
+            {
+                VocabularyFilterName = vocabularyFilterName
+            };
+
+            var data = await client.GetVocabularyFilterAsync(getRequest);
+
+            vocabularyFilter = data.VocabularyFilterName;
+        }
+
+        private void BtSearch_Click(object sender, EventArgs e)
+        {
+            openFileDialog.ShowDialog();
+            TextBox.Text = openFileDialog.FileName;
+        }
+
+        private async void BtJob_Click(object sender, EventArgs e)
+        {
+            await UpLoadToS3(openFileDialog.FileName);
+            await ExecuteTranscribeAsync();
+        }
+
+        private async void BtVocabularyFilter_Click(object sender, EventArgs e)
+        {
+            await ExecuteVocabularyFilter();
+        }
+    }
+}
